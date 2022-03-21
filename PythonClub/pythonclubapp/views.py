a0edad3b88451e81f449ff442f304b1af1e3d907
Matthieu Felker <77665167+Matthieu-ui@users.noreply.github.com>
2022-03-21 16:05:00 -0700

    Add files via upload

---
+++ PythonClub/pythonclubapp/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Meeting, Resource
+
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def index(request):
+	return render(request, 'pythonclubapp/index.html')
+
+def resources(request):
+	resourceList = Resource.objects.all()
+	return render(request, 'pythonclubapp/resources.html', {'resourceList': resourceList})
+
+def meetings(request):
+	meetingList=Meeting.objects.all()
+	return render(request,'pythonclubapp/meetings.html', {'meetingList': meetingList})
+
+def meetingDetails(request,id):
+	meeting=get_object_or_404(Meeting,pk=id)
+	return render(request,'pythonclubapp/meetingdetails.html', {'meeting': meeting})
+
+##form views
+@login_required
+def newMeeting(request):
+	form=MeetingForm
+
+	if request.method=='POST':
+		form=MeetingForm(request.POST)
+		if form.is_valid():
+			post=form.save(commit=True)
+			post.save()
+			form=MeetingForm
+	else:
+		form=MeetingForm()
+	return render(request, 'pythonclubapp/newmeeting.html', {'form':form})
+
+@login_required
+def newResource(request):
+	form=ResourceForm
+
+	if request.method=='POST':
+		form=ResourceForm(request.POST)
+		if form.is_valid():
+			post=form.save(commit=True)
+			post.save()
+			form=ResourceForm
+	else:
+		form=ResourceForm()
+	return render(request, 'pythonclubapp/newresource.html', {'form':form})
+
+def loginmessage(request):
+	return render(request,'pythonclubapp/loginmessage.html')
+
+def logoutmessage(request):
+	return render(request,'pythonclubapp/logoutmessage.html')
