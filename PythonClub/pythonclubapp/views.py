47f0bde0943fdb388a75331e8a83be978182fa0c
Matthieu Felker <77665167+Matthieu-ui@users.noreply.github.com>
2022-03-21 17:30:47 -0700

    update views

--- PythonClub/pythonclubapp/views.py
+++ PythonClub/pythonclubapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Meeting, Resource
-
+from .forms import MeetingForm,ResourceForm
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 def index(request):
