1b47231913e39113ae91333efdf42f9df04fceff
Matthieu Felker <77665167+Matthieu-ui@users.noreply.github.com>
2022-03-21 17:30:21 -0700

    update urls

--- PythonClub/pythonclubapp/urls.py
+++ PythonClub/pythonclubapp/urls.py
@@ -1,6 +1,12 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('',views.index, name='index'),
+    path('resources/',views.resources, name='resources'),
+    path('meetings/',views.meetings,name='meetings'),
+    path('meetingdetails/<int:id>', views.meetingDetails, name='meetingdetails'),
+    path('newmeeting/', views.newMeeting, name='newmeeting'),
+    path('newresource/', views.newResource, name='newresource'),
+    path('loginmessage/', views.loginmessage, name='loginmessage'),
+    path('logoutmessage/', views.logoutmessage, name='logoutmessage'),
 ]
